ff605f5cab8bc7c381590561b773d4fd8ce9ebb1
Xinquan XU <Xinquan.XU@partner.bmw.com>
2024-02-27 18:15:42 +0800

    Completed ARM64 ASM

---
+++ googleTest.cc
@@ -0,0 +1,112 @@
+#include "arm64asm.h"
+#include <cstdint>
+#include <stdlib.h>
+#include <gtest/gtest.h>
+
+// // Demonstrate some basic assertions.
+// TEST(myAbs, BasicAssertions) {
+//   // Expect two strings not to be equal.
+
+//   EXPECT_EQ(myAbs(-8), 8);
+//   EXPECT_EQ(myAbs(-4294967296), 4294967296);
+//   EXPECT_EQ(myAbs(8), 8);
+//   // Expect equality.
+//   EXPECT_EQ(myAbs(0), 0);
+//   EXPECT_EQ(myAbs(4294967296), 4294967296);
+// }
+
+// bool AssertMemset(void *p, int count, 
+// uint8_t pattern) {
+//     uint8_t *buf = (uint8_t*)p;
+//     for (int i = 0; i < count; i ++) {
+//         if (buf[i] != pattern) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// TEST(myMemset, BasicAssertions) {
+//     void *buffer = malloc(1024*1024*32);
+//     EXPECT_EQ(myMemset(buffer, 8, 0), 8);
+//     EXPECT_PRED3(AssertMemset, buffer, 8, 0);
+//     EXPECT_EQ(myMemset(buffer, 1024*1024*32, 0), 1024*1024*32);
+//     EXPECT_PRED3(AssertMemset, buffer, 1024*1024*32, 0);
+//     free(buffer);
+// }
+
+void PrintArray(int32_t *array, int counts) {
+    for (int i = 0; i < counts; i ++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void PrintArray2(int32_t *array, int begin, int end) {
+    for (int i = begin; i <= end; i ++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+bool AssertSort(int32_t *array, int counts) {
+    int32_t temp = array[0];
+    PrintArray(array, counts);
+    for (int i = 1; i < counts; i ++) {
+        if (array[i] < temp) {
+            printf("assert failed, arr[%d]=%d < arr[%d] = %d\n", i, array[i], i - 1, temp);
+            return false;
+        }
+        temp = array[i];
+    }
+    return true;
+}
+
+void QuickSort(int32_t *array, int begin, int end) {
+    if (begin >= end) {
+        return;
+    }
+    int32_t pivot = partition(array, begin, end);
+    printf("pivot=%d,\t", pivot);
+    PrintArray2(array, begin, end);
+    QuickSort(array, begin, pivot - 1);
+    QuickSort(array, pivot + 1, end);
+}
+
+
+// TEST(myGetSum, BasicAssertions) {
+//     EXPECT_EQ(myGetSum(0), 0);
+//     EXPECT_EQ(myGetSum(10), 55);
+//     EXPECT_EQ(myGetSum(256), 32896);
+//     EXPECT_EQ(myGetSum(16384), 134225920);
+//     EXPECT_EQ(myGetSum(4294967295), 9223372034707292160);
+// }
+
+
+
+TEST(myQuickSort, BasicAssertions) {
+    int raw[] = {6, 7, 8, 9, 0, 2, 3, 1, 4, 5};
+    // int32_t raw[] = {2, 3, 1};
+    int32_t raw_len = sizeof(raw) / sizeof(int32_t);
+    printf("array[%d]: ", raw_len);
+    PrintArray(raw, raw_len);
+    myQuickSort(raw, 0, raw_len - 1);
+    printf("after sort: ");
+    EXPECT_PRED2(AssertSort, (int*)raw, raw_len);
+}
+
+// #include <iostream>
+// #include <stdio.h>
+// // extern "C" uint64_t myAbs(int64_t x);
+// int main() {
+//     int raw[] = {-1, 5,5,5,6,7,8,9,15, 8772833};
+//     int raw_len = sizeof(raw) / sizeof(int);
+//     int res;
+//     myQuickSort(raw, 0, raw_len - 1);
+//     for (int i = 0; i < raw_len; i ++) {
+//         printf("%d: %d\n", i, raw[i]);
+//     }
+//     // std::cout << res;
+//     // printf("%d\n", res);
+// }
+
